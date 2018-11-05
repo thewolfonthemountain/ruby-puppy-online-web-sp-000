19d73b80efbaf563502db2b2735c2e36c5480ee3
Sean Langton <slangton1@gmail.com>
2018-11-05 22:28:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,13 @@
 class Dog
 
+attr_accessor :name
+
 @@all = []
+
+def initialize
+  @name = name
+  @@all << name
+end
 
 def self.clear_all
   @@all = []
@@ -9,6 +16,7 @@ end
 def self.all
   @@all.each {|dog| puts dog}
 end
+
 
 
 end
